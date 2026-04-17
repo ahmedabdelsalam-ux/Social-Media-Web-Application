6cae446a3a17bbf174961d00370065d6da7af952
ahmedabdelsalam-ux <ahmedabdelsalam552005@gmail.com>
2026-04-17 20:23:56 +0200

    third commit

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -4,9 +4,7 @@ import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(withRoutes(serverRoutes))
-  ]
+  providers: [provideServerRendering()],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
